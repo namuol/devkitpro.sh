bdc4a7444871c514190de378c9932e076c3a3356
louman <louman@jupiter>
2010-06-08 06:41:30 -0400

    - test.sh can now handle environments with no env. variables set (assuming that the devkitpro environment is in the current user's home directory ;-) )

--- test.sh
+++ test.sh
@@ -4,6 +4,13 @@
 #   Compile several example NDS programs to see if our environment is sane.
 #
 
+if [ -z $DEVKITPRO ]
+then
+    export DEVKITPRO=/home/louman/devkitpro
+    export DEVKITARM=$DEVKITPRO/devkitARM
+    export PAPATH=$DEVKITPRO/PAlib/lib
+fi
+
 red='\E[31;1m'
 green='\E[32;3m'
 
@@ -30,13 +37,16 @@ function checkForErrors() {
 }
 
 cd $DEVKITPRO/libnds/examples
+make clean
 make
 checkForErrors "Trouble building libnds examples. See above output for details."
 
 cd $DEVKITPRO/PAlib/examples/Demos/Frisbee/Frisbee3
+make clean
 make
 checkForErrors "Trouble building PAlib examples. See above output for details."
 
 cd $DEVKITPRO/uLibrary/Examples/Example06
+make clean
 make
 checkForErrors "Trouble building uLibrary examples. See above output for details."
