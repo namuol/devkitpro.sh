c7e23d88e44817eb745b2c509853f4d9de8d2c72
louman <louman@jupiter>
2010-07-08 21:34:44 -0400

    - devkitARM r31 is now out, and it appears that r30 has disappeared from sourceforge for some reason.

--- devkitpro.sh
+++ devkitpro.sh
@@ -37,10 +37,10 @@ export PAPATH=$DEVKITPRO'/PAlib/lib'
 # File URLS:
 if [ `uname -m` == "x86_64" ]
 then
-  DEVKITARM_URL="http://downloads.sourceforge.net/project/devkitpro/devkitARM/devkitARM_r30-x86_64-linux.tar.bz2"
+  DEVKITARM_URL="http://downloads.sourceforge.net/project/devkitpro/devkitARM/devkitARM_r31-x86_64-linux.tar.bz2"
 else
   # Default behavior
-  DEVKITARM_URL="http://downloads.sourceforge.net/project/devkitpro/devkitARM/devkitARM_r30-i686-linux.tar.bz2"
+  DEVKITARM_URL="http://downloads.sourceforge.net/project/devkitpro/devkitARM/devkitARM_r31-i686-linux.tar.bz2"
 fi
 DEFAULT_ARM7_URL="http://downloads.sourceforge.net/project/devkitpro/default%20arm7/default_arm7-0.5.12.tar.bz2"
 PALIB_URL="http://palib-dev.com/PAlib0912XX_Beta.7z"
