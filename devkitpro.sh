9e2e11c457233b39348f9640dedab66caecb6bf6
louman <louman@jupiter>
2010-09-27 13:25:54 -0400

    - Updated the following URLs (old versions are no longer archived on sourceforge, it   seems):  * libnds (1.4.5 -> 1.4.6)  * default_arm7 (0.5.15 -> 0.5.16)  * dswifi (0.3.12 -> 0.3.13)

--- devkitpro.sh
+++ devkitpro.sh
@@ -42,14 +42,14 @@ else
   # Default behavior
   DEVKITARM_URL="http://downloads.sourceforge.net/project/devkitpro/devkitARM/devkitARM_r31-i686-linux.tar.bz2"
 fi
-DEFAULT_ARM7_URL="http://downloads.sourceforge.net/project/devkitpro/default%20arm7/default_arm7-0.5.15.tar.bz2"
+DEFAULT_ARM7_URL="http://downloads.sourceforge.net/project/devkitpro/default%20arm7/default_arm7-0.5.16.tar.bz2"
 PALIB_URL="http://palib-dev.com/PAlib100707.7z"
-LIBNDS_URL="http://downloads.sourceforge.net/project/devkitpro/libnds/libnds-1.4.5.tar.bz2"
+LIBNDS_URL="http://downloads.sourceforge.net/project/devkitpro/libnds/libnds-1.4.6.tar.bz2"
 MAXMOD_URL="http://downloads.sourceforge.net/project/devkitpro/maxmod/maxmod%201.0.6/maxmod-nds-1.0.6.tar.bz2"
 LIBNDS_EX_URL="http://downloads.sourceforge.net/project/devkitpro/examples/nds/nds-examples-20100724.tar.bz2"
 LIBFAT_URL="http://downloads.sourceforge.net/project/devkitpro/libfat/libfat-nds-1.0.7.tar.bz2"
 LIBFILESYSTEM_URL="http://downloads.sourceforge.net/project/devkitpro/filesystem/libfilesystem-0.9.6.tar.bz2"
-DSWIFI_URL="http://downloads.sourceforge.net/project/devkitpro/dswifi/dswifi-0.3.12.tar.bz2"
+DSWIFI_URL="http://downloads.sourceforge.net/project/devkitpro/dswifi/dswifi-0.3.13.tar.bz2"
 NOCASHGBA_URL="http://nocash.emubase.de/no\$gba-w.zip"
 ULIB_URL="http://brunni.dev-fr.org/dl/nds/uLibrary.7z"
 
